df678211217e3507340166f364f9c73868f8c5da
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-22 23:29:29 +0500

    Solved ChattyBotWelcomesYou problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/ChattyBotWelcomesYou.java
@@ -0,0 +1,7 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Projects;
+
+public class ChattyBotWelcomesYou {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is AniMarfo.\nI was created in 2021.");
+    }
+}
