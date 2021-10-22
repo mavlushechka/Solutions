120219d68bd806e9a4725e068b37f0176ebf53d4
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-22 23:26:50 +0500

    Solved IntroductionToJavaHello problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Daily/IntroductionToJavaHello.java
@@ -0,0 +1,7 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Daily;
+
+public class IntroductionToJavaHello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Mavlushechka!");
+    }
+}
