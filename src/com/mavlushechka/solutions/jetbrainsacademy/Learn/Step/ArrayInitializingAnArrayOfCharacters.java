df2d549dfbe7cbbaf192bda991ccf4a4d4f084c9
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-24 03:35:25 +0500

    Added recommended new empty line after variable

--- src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/ArrayInitializingAnArrayOfCharacters.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/ArrayInitializingAnArrayOfCharacters.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class ArrayInitializingAnArrayOfCharacters {
     public static void main(String[] args) {
         char[] characters = {'a', 'z', 'e', 'd'};
+
         System.out.println(Arrays.toString(characters));
     }
 }
