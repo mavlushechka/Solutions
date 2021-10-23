5c032a758bd405d9a180a73596def68e260dafeb
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-23 10:40:10 +0500

    Solved OverviewOfTheBasicProgramCatsAreRobots problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Daily/OverviewOfTheBasicProgramCatsAreRobots.java
@@ -0,0 +1,7 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Daily;
+
+public class OverviewOfTheBasicProgramCatsAreRobots {
+    public static void main(String[] args) {
+        System.out.println("Cats are robots!");
+    }
+}
