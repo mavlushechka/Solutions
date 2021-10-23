fba59ba6e419a215548f7b7b10b4d08aab2f6343
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-24 03:20:38 +0500

    Solved ArrayInitializingAnArrayOfCharacters problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/ArrayInitializingAnArrayOfCharacters.java
@@ -0,0 +1,10 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+import java.util.Arrays;
+
+public class ArrayInitializingAnArrayOfCharacters {
+    public static void main(String[] args) {
+        char[] characters = {'a', 'z', 'e', 'd'};
+        System.out.println(Arrays.toString(characters));
+    }
+}
