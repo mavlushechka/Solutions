abf4a2228de9e238eb77c49289782fc1f57f2d7a
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-23 11:47:29 +0500

    Deleted ChattyBotWelcomesYou

--- src/com/mavlushechka/solutions/jetbrainsacademy/Projects/ChattyBotWelcomesYou.java
+++
@@ -1,7 +0,0 @@
-package com.mavlushechka.solutions.jetbrainsacademy.Projects;
-
-public class ChattyBotWelcomesYou {
-    public static void main(String[] args) {
-        System.out.println("Hello! My name is AniMarfo.\nI was created in 2021.");
-    }
-}
