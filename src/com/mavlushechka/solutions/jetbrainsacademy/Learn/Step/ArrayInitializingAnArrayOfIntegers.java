48bcee8dd112d8764b6f17e87dea3b448ce41a1b
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-24 03:34:00 +0500

    Solved ArrayInitializingAnArrayOfIntegers problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/ArrayInitializingAnArrayOfIntegers.java
@@ -0,0 +1,11 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+import java.util.Arrays;
+
+public class ArrayInitializingAnArrayOfIntegers {
+    public static void main(String[] args) {
+        int[] numbers = {12, 17, 8, 101, 33};
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
