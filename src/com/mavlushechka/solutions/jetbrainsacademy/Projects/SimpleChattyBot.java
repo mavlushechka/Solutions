035477b8a056f367b46ed3ea04c2d86111621c4b
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-23 21:48:05 +0500

    Solved 5/5 stage

--- src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleChattyBot.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleChattyBot.java
@@ -3,30 +3,59 @@ package com.mavlushechka.solutions.jetbrainsacademy.Projects;
 import java.util.Scanner;
 
 public class SimpleChattyBot {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        byte remainder3, remainder5, remainder7;
-        byte age;
-        int number;
+    final static Scanner scanner = new Scanner(System.in);
 
-        System.out.println("""
-                Hello! My name is AniMarfo.
-                I was created in 2021.""");
+    public static void main(String[] args) {
+        greet("AniMarfo", "2021");
+        remindName();
+        guessAge();
+        count();
+        test();
+        end();
+    }
+
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
-        System.out.println("What a great name you have, " + scanner.next() + "!");
-        System.out.println("""
-                Let me guess your age.
-                Enter remainders of dividing your age by 3, 5 and 7.""");
-        remainder3 = scanner.nextByte();
-        remainder5 = scanner.nextByte();
-        remainder7 = scanner.nextByte();
-        age = (byte) ((remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105);
+    }
+
+    static void remindName() {
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
+    }
+
+    static void guessAge() {
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
+
+    static void count() {
         System.out.println("Now I will prove to you that I can count to any number you want.");
-        number = scanner.nextInt();
-        for (int i = 0; i <= number; i++) {
-            System.out.printf("%s!\n", i);
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
         }
-        System.out.println("Completed, have a nice day!");
+    }
+
+    static void test() {
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("1. To repeat a statement multiple times.");
+        System.out.println("2. To decompose a program into several small subroutines.");
+        System.out.println("3. To determine the execution time of a program.");
+        System.out.println("4. To interrupt the execution of a program.");
+
+        while (scanner.nextByte() != 2) {
+            System.out.println("Please, try again.");
+        }
+    }
+
+    static void end() {
+        System.out.println("Congratulations, have a nice day!");
     }
 }
