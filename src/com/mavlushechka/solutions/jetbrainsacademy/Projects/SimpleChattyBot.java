9fd1a35aefcebbcc7c05525b3ed5066e7adc95d9
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-23 21:38:53 +0500

    Solved 4/5 stage

--- src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleChattyBot.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleChattyBot.java
@@ -7,12 +7,13 @@ public class SimpleChattyBot {
         Scanner scanner = new Scanner(System.in);
         byte remainder3, remainder5, remainder7;
         byte age;
+        int number;
 
         System.out.println("""
                 Hello! My name is AniMarfo.
                 I was created in 2021.""");
         System.out.println("Please, remind me your name.");
-        System.out.printf("What a great name you have, %s!\n", new Scanner(System.in).next());
+        System.out.println("What a great name you have, " + scanner.next() + "!");
         System.out.println("""
                 Let me guess your age.
                 Enter remainders of dividing your age by 3, 5 and 7.""");
@@ -20,6 +21,12 @@ public class SimpleChattyBot {
         remainder5 = scanner.nextByte();
         remainder7 = scanner.nextByte();
         age = (byte) ((remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105);
-        System.out.printf("Your age is %s; that's a good time to start programming!", age);
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        number = scanner.nextInt();
+        for (int i = 0; i <= number; i++) {
+            System.out.printf("%s!\n", i);
+        }
+        System.out.println("Completed, have a nice day!");
     }
 }
