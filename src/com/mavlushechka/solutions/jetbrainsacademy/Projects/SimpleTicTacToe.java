e98d185c1f2ff24a0095e8b8becdb2aff5a12d19
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-24 02:57:39 +0500

    Solved 1/5 stage

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleTicTacToe.java
@@ -0,0 +1,10 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Projects;
+
+public class SimpleTicTacToe {
+    public static void main(String[] args) {
+        System.out.println("""
+                X O X
+                O X O
+                X X O""");
+    }
+}
