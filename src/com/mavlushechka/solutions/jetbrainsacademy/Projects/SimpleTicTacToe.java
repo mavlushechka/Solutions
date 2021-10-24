5f7b82b1d995062bcb66e0742f969987d396e6f3
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-24 15:48:43 +0500

    Solved 2/5 stage

--- src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleTicTacToe.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleTicTacToe.java
@@ -1,10 +1,28 @@
 package com.mavlushechka.solutions.jetbrainsacademy.Projects;
 
+import java.util.Scanner;
+
 public class SimpleTicTacToe {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static char[] cells;
+
     public static void main(String[] args) {
-        System.out.println("""
-                X O X
-                O X O
-                X X O""");
+        enterCells();
+        showGrid();
+    }
+
+    private static void enterCells() {
+        System.out.print("Enter cells: ");
+        cells = scanner.nextLine().toCharArray();
+    }
+
+    private static void showGrid() {
+        for (byte i = 0; i < 5; i++) {
+            if (i == 0 || i == 4) {
+                System.out.println("---------");
+            } else {
+                System.out.println("| " + cells[(i-1)*3] + " " + cells[(i-1)*3+1] + " " + cells[(i-1)*3+2] + " |");
+            }
+        }
     }
 }
