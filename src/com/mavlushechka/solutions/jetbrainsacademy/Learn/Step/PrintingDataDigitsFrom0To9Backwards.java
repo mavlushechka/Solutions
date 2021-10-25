cc32b45b6910ca80fba6444acaf8f1172eb3e5f2
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-26 00:50:15 +0500

    Solved PrintingDataDigitsFrom0To9Backwards problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/PrintingDataDigitsFrom0To9Backwards.java
@@ -0,0 +1,9 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+public class PrintingDataDigitsFrom0To9Backwards {
+    public static void main(String[] args) {
+        for (byte i = 9; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
