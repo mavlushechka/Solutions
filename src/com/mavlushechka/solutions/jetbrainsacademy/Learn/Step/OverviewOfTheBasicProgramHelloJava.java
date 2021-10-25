69a3951049cae3376862778949c71689e6857c73
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-26 00:27:19 +0500

    Solved OverviewOfTheBasicProgramHelloJava problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/OverviewOfTheBasicProgramHelloJava.java
@@ -0,0 +1,7 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+public class OverviewOfTheBasicProgramHelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}
