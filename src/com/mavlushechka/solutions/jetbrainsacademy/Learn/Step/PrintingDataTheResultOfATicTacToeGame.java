d23aaeff9df04d723a320532f2f61885dd3d8eec
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-26 01:00:13 +0500

    Solved PrintingDataTheResultOfATicTacToeGame problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/PrintingDataTheResultOfATicTacToeGame.java
@@ -0,0 +1,7 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+public class PrintingDataTheResultOfATicTacToeGame {
+    public static void main(String[] args) {
+        System.out.print("O X X\nO X O\nX O X");
+    }
+}
