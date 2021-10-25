4ad42526a37070ea34203616620258323027a2ae
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-25 10:07:19 +0500

    Added recommended empty line

--- src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleChattyBot.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleChattyBot.java
@@ -3,6 +3,7 @@ package com.mavlushechka.solutions.jetbrainsacademy.Projects;
 import java.util.Scanner;
 
 public class SimpleChattyBot {
+
     final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
