256b721517d6e9d74f71e40fa30b224a24a04315
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-26 00:44:53 +0500

    Solved PrintingDataPrintingAMagicLetter problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/PrintingDataPrintingAMagicLetter.java
@@ -0,0 +1,10 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+public class PrintingDataPrintingAMagicLetter {
+    public static void main(String[] args) {
+        System.out.print("********\n");
+        System.out.print("      **\n".repeat(3));
+        System.out.print("**    **\n".repeat(2));
+        System.out.print(" *******\n");
+    }
+}
