436a6521d383b613458a73318d7d4c30ccedce92
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-25 11:37:10 +0500

    Solved ErrorsInProgramsFixTheProgram problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/ErrorsInProgramsFixTheProgram.java
@@ -0,0 +1,15 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+import java.util.Scanner;
+
+public class ErrorsInProgramsFixTheProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int sum = a + b + c;
+
+        System.out.println(sum);
+    }
+}
