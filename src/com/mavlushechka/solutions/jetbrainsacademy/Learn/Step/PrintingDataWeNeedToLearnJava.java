106e76590ac00e6f1b1f51dc72344b2b43dd0d94
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-26 00:57:54 +0500

    Solved PrintingDataWeNeedToLearnJava problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/PrintingDataWeNeedToLearnJava.java
@@ -0,0 +1,7 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+public class PrintingDataWeNeedToLearnJava {
+    public static void main(String[] args) {
+        System.out.print("WE NEED TO\n\nLEARN JAVA\n\nAS QUICKLY AS POSSIBLE");
+    }
+}
