2b448d4b50d3107d625aedee3131c5490c7e5728
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-27 19:09:47 +0500

    Solved 4/5 stage

--- src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleTicTacToe.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleTicTacToe.java
@@ -9,6 +9,12 @@ public final class SimpleTicTacToe {
 
     private static final char[][] cells = new char[3][3];
 
+    private static char winner;
+
+    private static byte countOfX;
+
+    private static byte countOfO;
+
     public static void main(String[] args) {
         enterCells();
         showGrid();
@@ -30,15 +36,13 @@ public final class SimpleTicTacToe {
     }
 
     private static void check() {
-        char winner = 0;
         byte numberOfWins = 0;
-        byte countOfX = 0;
-        byte countOfO = 0;
         boolean gameFinished = !(Arrays.toString(cells[0]).contains("_")
                 || Arrays.toString(cells[1]).contains("_")
                 || Arrays.toString(cells[2]).contains("_"));
+
         for (byte i = 0, j = 2; i < 3; i += 2, j -= 2) {
-            if (cells[i][0] == cells[1][1] && cells[1][1] == cells[j][2]) {
+            if ((cells[i][0] != '_' && cells[i][0] != ' ') && (cells[i][0] == cells[1][1] && cells[1][1] == cells[j][2])) {
                 gameFinished = true;
                 winner = cells[0][0];
                 numberOfWins++;
@@ -53,8 +57,8 @@ public final class SimpleTicTacToe {
             }
         }
         for (byte i = 0; i < 3; i++) {
-            if ((cells[i][0] == cells[i][1] && cells[i][1] == cells[i][2]) ||
-                    (cells[0][i] == cells[1][i] && cells[1][i] == cells[2][i])) {
+            if ((cells[i][0] != '_' && cells[i][0] != ' ') && ((cells[i][0] == cells[i][1] && cells[i][1] == cells[i][2]) ||
+                    (cells[0][i] == cells[1][i] && cells[1][i] == cells[2][i]))) {
                 gameFinished = true;
                 winner = ((cells[i][0] == cells[i][1] && cells[i][1] == cells[i][2])) ? cells[i][0] : cells[0][i];
                 numberOfWins++;
@@ -70,7 +74,34 @@ public final class SimpleTicTacToe {
                 default -> System.out.println("Impossible");
             }
         } else {
-            System.out.println("Game not finished");
+            enter();
+            showGrid();
+        }
+    }
+
+    private static void enter() {
+        byte y;
+        byte x;
+
+        System.out.print("Enter the coordinates: ");
+        try {
+            String tempCoordinates = scanner.nextLine();
+            y = Byte.parseByte(tempCoordinates.split(" ")[0]);
+            x = Byte.parseByte(tempCoordinates.split(" ")[1]);
+        } catch (Exception exception) {
+            System.out.println("You should enter numbers!");
+            enter();
+            return;
+        }
+
+        if (!((y >= 1 && y <= 3) && (x >= 1 && x <= 3))) {
+            System.out.println("Coordinates should be from 1 to 3!");
+            enter();
+        } else if (cells[y-1][x-1] != '_' && cells[y-1][x-1] != ' ') {
+            System.out.println("This cell is occupied! Choose another one!");
+            enter();
+        } else {
+            cells[y-1][x-1] = 'X';
         }
     }
 }
