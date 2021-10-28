3d44322a03a57dd24dad6ba315cabb144390854b
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-28 23:11:22 +0500

    Solved 2/5 stage

--- src/com/mavlushechka/solutions/jetbrainsacademy/Projects/CinemaRoomManager.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/CinemaRoomManager.java
@@ -1,30 +1,69 @@
 package com.mavlushechka.solutions.jetbrainsacademy.Projects;
 
+import java.util.Scanner;
+
 public final class CinemaRoomManager {
 
-    private final byte ROWS = 7;
+    private final byte rows;
 
-    private final byte SEATS_IN_ONE_ROW = 8;
+    private final byte seatsInEachRow;
+
+    private final boolean largeRoom;
+
+    private final byte MAX_SEATS_IN_SMALL_ROOM = 60;
 
     public static void main(String[] args) {
-        CinemaRoomManager cinemaRoomManager = new CinemaRoomManager();
-        System.out.println("Cinema:");
-        cinemaRoomManager.showRoom();
+        CinemaRoomManager cinemaRoomManager;
+        Scanner scanner = new Scanner(System.in);
+        byte rows;
+        byte seatsInEachRow;
+
+        System.out.println("Enter the number of rows:");
+        rows = scanner.nextByte();
+        System.out.println("Enter the number of seats in each row:");
+        seatsInEachRow = scanner.nextByte();
+        cinemaRoomManager = new CinemaRoomManager(rows, seatsInEachRow);
+
+        System.out.println("Total income:\n" + cinemaRoomManager.getIncome());
+    }
+
+    public CinemaRoomManager(byte rows, byte seatsInEachRow) {
+        this.rows = rows;
+        this.seatsInEachRow = seatsInEachRow;
+        largeRoom = rows * seatsInEachRow > MAX_SEATS_IN_SMALL_ROOM;
     }
 
     public void showRoom() {
-        for (int i = 0; i <= SEATS_IN_ONE_ROW; i++) {
+        for (int i = 0; i <= seatsInEachRow; i++) {
             System.out.printf((i != 0) ? ("%d ") : ("  "), i);
         }
         System.out.println();
-        for (int i = 1; i <= ROWS; i++) {
+        for (int i = 1; i <= rows; i++) {
             System.out.print(i);
-            for (int j = 1; j <= SEATS_IN_ONE_ROW; j++) {
+            for (int j = 1; j <= seatsInEachRow; j++) {
                 System.out.print(" S");
             }
-            if (i != ROWS) {
+            if (i != rows) {
                 System.out.println();
             }
         }
     }
+
+    public String getIncome() {
+        int income = (int) ((!largeRoom) ? Ticket.PRICE_DEFAULT * rows * seatsInEachRow :
+                seatsInEachRow * ((Ticket.PRICE_FOR_FRONT_HALF_OF_ROOM * Math.floor(rows / 2.0)) +
+                (Ticket.PRICE_FOR_BACK_HALF_OF_ROOM * Math.ceil(rows / 2.0))));
+        return Ticket.PRICE_CURRENCY + Integer.toString(income);
+    }
+}
+
+abstract class Ticket {
+
+    public static char PRICE_CURRENCY = '$';
+
+    public static byte PRICE_DEFAULT = 10;
+
+    public static byte PRICE_FOR_FRONT_HALF_OF_ROOM = 10;
+
+    public static byte PRICE_FOR_BACK_HALF_OF_ROOM = 8;
 }
