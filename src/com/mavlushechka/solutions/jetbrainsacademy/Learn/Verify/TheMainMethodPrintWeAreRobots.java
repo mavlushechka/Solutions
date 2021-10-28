2b41f3131296f2f83aedf5b8ccda99640d2cca3f
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-29 01:33:49 +0500

    Solved TheMainMethodPrintWeAreRobots problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Verify/TheMainMethodPrintWeAreRobots.java
@@ -0,0 +1,8 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Verify;
+
+public class TheMainMethodPrintWeAreRobots {
+
+    public static void main(String[] args) {
+        System.out.println("We are robots!");
+    }
+}
