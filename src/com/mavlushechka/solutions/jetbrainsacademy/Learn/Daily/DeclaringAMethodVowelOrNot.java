8d1c363466e1696ffe2c770ff8efcac824e140cc
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-29 10:29:21 +0500

    Solved DeclaringAMethodVowelOrNot problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Daily/DeclaringAMethodVowelOrNot.java
@@ -0,0 +1,19 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Daily;
+
+import java.util.Scanner;
+import java.util.Set;
+
+public class DeclaringAMethodVowelOrNot {
+
+    private final static Set<Character> VOWEL_LETTERS = Set.of('A', 'E', 'I', 'O', 'U', 'W');
+
+    public static boolean isVowel(char letter) {
+        return VOWEL_LETTERS.contains(Character.toUpperCase(letter));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char letter = scanner.nextLine().charAt(0);
+        System.out.println(isVowel(letter) ? "YES" : "NO");
+    }
+}
