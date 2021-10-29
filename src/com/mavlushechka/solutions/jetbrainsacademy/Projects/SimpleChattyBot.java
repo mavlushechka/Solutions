62c6c9b5cb78c787728fd4a062575cbf2341901d
mavlonerkinboev <mavlonbek.ibragimov007@gmail.com>
2021-10-30 00:10:15 +0500

    Renamed num variable to number. Changed System.out.println to System.out.printf

--- src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleChattyBot.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/SimpleChattyBot.java
@@ -39,9 +39,9 @@ public class SimpleChattyBot {
 
     private void count() {
         System.out.println("Now I will prove to you that I can count to any number you want.");
-        int num = scanner.nextInt();
-        for (int i = 0; i <= num; i++) {
-            System.out.println(i + "!");
+        int number = scanner.nextInt();
+        for (int i = 0; i <= number; i++) {
+            System.out.printf("%d!\n", i);
         }
     }
 
