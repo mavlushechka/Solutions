106ea5113e0ab1625321db729fbbed07d2f39c4f
mavlushechka <mavlushechka@gmail.com>
2021-11-18 11:01:18 +0500

    Solved 8.1st laboratory

---
+++ src/com/mavlushechka/solutions/university/_10_1/_8.java
@@ -0,0 +1,19 @@
+package com.mavlushechka.solutions.university._10_1;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _8 {
+
+    public static void main(String[] args) {
+        ArrayDeque<Character> characters = new ArrayDeque<>();
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        for (int i = 0; i < size; i++) {
+            characters.push(scanner.next().charAt(0));
+            if (i != 0 && characters.toArray()[0] == characters.toArray()[1]) {
+                System.out.print(characters.toArray()[0] + " ");
+            }
+        }
+    }
+}
