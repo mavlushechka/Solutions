0143664e5a8c059d47ec80e7983ed51d8094995f
mavlushechka <mavlushechka@gmail.com>
2021-11-22 11:36:29 +0500

    Solved ConditionalStatementCheckANumberIsPositive solution

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Daily/ConditionalStatementCheckANumberIsPositive.java
@@ -0,0 +1,10 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Daily;
+
+import java.util.Scanner;
+
+public class ConditionalStatementCheckANumberIsPositive {
+
+    public static void main(String[] args) {
+        System.out.println(new Scanner(System.in).nextInt() > 0 ? "YES" : "NO");
+    }
+}
