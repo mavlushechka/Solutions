a7abfab495c5dd4d987e2a815b91117a5d54b710
Mavlushechka <mavlushechka@gmail.com>
2021-12-10 22:54:45 +0500

    Solve 4/6 stage

--- src/com/mavlushechka/solutions/jetbrainsacademy/Projects/CoffeeMachine.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/CoffeeMachine.java
@@ -4,54 +4,102 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
 
-    private int waterMilliliters;
+    private int waterMilliliters = 400;
 
-    private int milkMilliliters;
+    private int milkMilliliters = 540;
 
-    private int coffeeBeansGram;
+    private int coffeeBeansGram = 120;
+
+    private int disposableCupsNumber = 9;
+
+    private int moneyAmount = 550;
 
     public void start() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Write how many ml of water the coffee machine has: ");
-        waterMilliliters = scanner.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has: ");
-        milkMilliliters = scanner.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
-        coffeeBeansGram = scanner.nextInt();
-        System.out.println("Write how many cups of coffee you will need: ");
-        calculateIfCanMakeCups(scanner.nextInt());
+        showInformation();
+        executeAction();
+        System.out.println();
+        showInformation();
     }
 
-    private void calculateIfCanMakeCups(int cupsNumber) {
-        int cupsNumberWithWater = waterMilliliters / CoffeeCup.WATER.getQuantity();
-        int cupsNumberWithMilk = milkMilliliters / CoffeeCup.MILK.getQuantity();
-        int cupsNumberWithCoffeeBeans = coffeeBeansGram / CoffeeCup.COFFEE_BEANS.getQuantity();
+    private void showInformation() {
+        System.out.printf("""
+                The coffee machine has:
+                %d ml of water
+                %d ml of milk
+                %d g of coffee beans
+                %d disposable cups
+                $%d of money
+                """, waterMilliliters, milkMilliliters, coffeeBeansGram, disposableCupsNumber, moneyAmount);
+    }
 
-        int cupsNumberMachineCanMake = Math.min(cupsNumberWithWater, Math.min(cupsNumberWithMilk, cupsNumberWithCoffeeBeans));
+    private void executeAction() {
+        Scanner scanner = new Scanner(System.in);
 
-        if (cupsNumberMachineCanMake > cupsNumber) {
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)\n", cupsNumberMachineCanMake - cupsNumber);
-        } else if (cupsNumberMachineCanMake == cupsNumber) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else {
-            System.out.printf("No, I can make only %d cup(s) of coffee\n", cupsNumberMachineCanMake);
+        System.out.println("\nWrite action (buy, fill, take):");
+        switch (Action.valueOf(scanner.next().toUpperCase())) {
+            case BUY -> buy();
+            case FILL -> fill();
+            case TAKE -> take();
         }
     }
 
-}
+    private void buy() {
+        Scanner scanner = new Scanner(System.in);
 
-enum CoffeeCup {
-    WATER(200), MILK(50), COFFEE_BEANS(15);
-
-    private final int quantity;
-
-    CoffeeCup(int quantity) {
-        this.quantity = quantity;
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+        CoffeeType coffeeType = switch (scanner.nextInt()) {
+            case 1 -> CoffeeType.ESPRESSO;
+            case 2 -> CoffeeType.LATTE;
+            default -> CoffeeType.CAPPUCCINO;
+        };
+        waterMilliliters -= coffeeType.WATER_MILLILITERS;
+        milkMilliliters -= coffeeType.MILK_MILLILITERS;
+        coffeeBeansGram -= coffeeType.COFFEE_BEANS_GRAM;
+        moneyAmount += coffeeType.COST;
+        disposableCupsNumber--;
     }
 
-    public int getQuantity() {
-        return this.quantity;
+    private void fill() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many ml of water you want to add:");
+        waterMilliliters += scanner.nextInt();
+        System.out.println("Write how many ml of milk you want to add:");
+        milkMilliliters += scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans you want to add:");
+        coffeeBeansGram += scanner.nextInt();
+        System.out.println("Write how many disposable cups of coffee you want to add: ");
+        disposableCupsNumber += scanner.nextInt();
+    }
+
+    private void take() {
+        System.out.println("I gave you $" + moneyAmount);
+        moneyAmount = 0;
+    }
+}
+
+enum Action {
+    BUY, FILL, TAKE
+}
+
+enum CoffeeType {
+    ESPRESSO(250, 0, 16, 4),
+    LATTE(350, 75, 20, 7),
+    CAPPUCCINO(200, 100, 12, 6);
+
+    public final int WATER_MILLILITERS;
+
+    public final int MILK_MILLILITERS;
+
+    public final int COFFEE_BEANS_GRAM;
+
+    public final int COST;
+
+    CoffeeType(int waterMilliliters, int milkMilliliters, int coffeeBeansGram, int cost) {
+        WATER_MILLILITERS = waterMilliliters;
+        MILK_MILLILITERS = milkMilliliters;
+        COFFEE_BEANS_GRAM = coffeeBeansGram;
+        COST = cost;
     }
 }
 
