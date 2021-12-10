8ac561ed1832d0fb58b8bc2671307ca8d278a589
Mavlushechka <mavlushechka@gmail.com>
2021-12-10 13:21:39 +0500

    Solved 2/5 stage

--- src/com/mavlushechka/solutions/jetbrainsacademy/Projects/CoffeeMachine.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/CoffeeMachine.java
@@ -1,15 +1,21 @@
 package com.mavlushechka.solutions.jetbrainsacademy.Projects;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
 
     public static void main(String[] args) {
-        System.out.println("""
-                Starting to make a coffee
-                Grinding coffee beans
-                Boiling water
-                Mixing boiled water with crushed coffee beans
-                Pouring coffee into the cup
-                Pouring some milk into the cup
-                Coffee is ready!""");
+        System.out.println("Write how many cups of coffee you will need: ");
+        int cupsNumber = new Scanner(System.in).nextInt();
+        int waterMilliliters = cupsNumber * 200;
+        int milkMilliliters = cupsNumber * 50;
+        int beansGram = cupsNumber * 15;
+
+        System.out.printf("""
+                For %d cups of coffee you will need:
+                %d ml of water
+                %d ml of milk
+                %d g of coffee beans
+                """, cupsNumber, waterMilliliters, milkMilliliters, beansGram);
     }
 }
