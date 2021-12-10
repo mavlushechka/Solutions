565b343fc88968c7416f52a0b0d9e42dcefcf0cc
Mavlushechka <mavlushechka@gmail.com>
2021-12-10 21:26:46 +0500

    Solve 3/6 stage

--- src/com/mavlushechka/solutions/jetbrainsacademy/Projects/CoffeeMachine.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/CoffeeMachine.java
@@ -4,18 +4,61 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
 
-    public static void main(String[] args) {
-        System.out.println("Write how many cups of coffee you will need: ");
-        int cupsNumber = new Scanner(System.in).nextInt();
-        int waterMilliliters = cupsNumber * 200;
-        int milkMilliliters = cupsNumber * 50;
-        int beansGram = cupsNumber * 15;
+    private int waterMilliliters;
 
-        System.out.printf("""
-                For %d cups of coffee you will need:
-                %d ml of water
-                %d ml of milk
-                %d g of coffee beans
-                """, cupsNumber, waterMilliliters, milkMilliliters, beansGram);
+    private int milkMilliliters;
+
+    private int coffeeBeansGram;
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        waterMilliliters = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        milkMilliliters = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        coffeeBeansGram = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need: ");
+        calculateIfCanMakeCups(scanner.nextInt());
+    }
+
+    private void calculateIfCanMakeCups(int cupsNumber) {
+        int cupsNumberWithWater = waterMilliliters / CoffeeCup.WATER.getQuantity();
+        int cupsNumberWithMilk = milkMilliliters / CoffeeCup.MILK.getQuantity();
+        int cupsNumberWithCoffeeBeans = coffeeBeansGram / CoffeeCup.COFFEE_BEANS.getQuantity();
+
+        int cupsNumberMachineCanMake = Math.min(cupsNumberWithWater, Math.min(cupsNumberWithMilk, cupsNumberWithCoffeeBeans));
+
+        if (cupsNumberMachineCanMake > cupsNumber) {
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)\n", cupsNumberMachineCanMake - cupsNumber);
+        } else if (cupsNumberMachineCanMake == cupsNumber) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else {
+            System.out.printf("No, I can make only %d cup(s) of coffee\n", cupsNumberMachineCanMake);
+        }
+    }
+
+}
+
+enum CoffeeCup {
+    WATER(200), MILK(50), COFFEE_BEANS(15);
+
+    private final int quantity;
+
+    CoffeeCup(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+}
+
+class Person {
+
+    public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.start();
     }
 }
