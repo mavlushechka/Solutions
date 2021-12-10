f27b0579f47b44e48540f1b0c323943f6f8235d7
Mavlushechka <mavlushechka@gmail.com>
2021-12-10 12:10:30 +0500

    Solved TheMainMethodPrintHelloJava problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Daily/TheMainMethodPrintHelloJava.java
@@ -0,0 +1,8 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Daily;
+
+public class TheMainMethodPrintHelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}
