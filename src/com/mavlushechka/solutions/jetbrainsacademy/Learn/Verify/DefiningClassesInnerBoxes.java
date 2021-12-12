9cc5cd2d6cba3386679b576a444baa4bb3662bbe
Mavlushechka <mavlushechka@gmail.com>
2021-12-12 21:12:15 +0500

    Solve DefiningClassesInnerBoxes problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Verify/DefiningClassesInnerBoxes.java
@@ -0,0 +1,16 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Verify;
+
+public class DefiningClassesInnerBoxes {
+
+}
+
+class Box {
+
+    public double length;
+
+    public double height;
+
+    public double width;
+
+    public Box innerBox;
+}
