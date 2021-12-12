2ecb96866a6b4531a759758c74bbc58836f02047
Mavlushechka <mavlushechka@gmail.com>
2021-12-12 15:45:40 +0500

    Solve 5/6 stage

--- src/com/mavlushechka/solutions/jetbrainsacademy/Projects/CoffeeMachine.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Projects/CoffeeMachine.java
@@ -1,5 +1,6 @@
 package com.mavlushechka.solutions.jetbrainsacademy.Projects;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CoffeeMachine {
@@ -15,54 +16,59 @@ public class CoffeeMachine {
     private int moneyAmount = 550;
 
     public void start() {
-        showInformation();
         executeAction();
-        System.out.println();
-        showInformation();
-    }
-
-    private void showInformation() {
-        System.out.printf("""
-                The coffee machine has:
-                %d ml of water
-                %d ml of milk
-                %d g of coffee beans
-                %d disposable cups
-                $%d of money
-                """, waterMilliliters, milkMilliliters, coffeeBeansGram, disposableCupsNumber, moneyAmount);
     }
 
     private void executeAction() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\nWrite action (buy, fill, take):");
-        switch (Action.valueOf(scanner.next().toUpperCase())) {
-            case BUY -> buy();
-            case FILL -> fill();
-            case TAKE -> take();
+        cycle: while (true) {
+            System.out.println("Write action (buy, fill, take, remaining, exit):");
+            switch (Action.valueOf(scanner.next().toUpperCase())) {
+                case BUY -> buy();
+                case FILL -> fill();
+                case TAKE -> take();
+                case REMAINING -> showInformation();
+                case EXIT -> {
+                    break cycle;
+                }
+            }
+            System.out.println();
         }
     }
 
     private void buy() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-        CoffeeType coffeeType = switch (scanner.nextInt()) {
-            case 1 -> CoffeeType.ESPRESSO;
-            case 2 -> CoffeeType.LATTE;
-            default -> CoffeeType.CAPPUCCINO;
-        };
-        waterMilliliters -= coffeeType.WATER_MILLILITERS;
-        milkMilliliters -= coffeeType.MILK_MILLILITERS;
-        coffeeBeansGram -= coffeeType.COFFEE_BEANS_GRAM;
-        moneyAmount += coffeeType.COST;
-        disposableCupsNumber--;
+        System.out.println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+        String action = new Scanner(System.in).next();
+        if (!Objects.equals(action, "back")) {
+            CoffeeType coffeeType = switch (Integer.parseInt(action)) {
+                case 1 -> CoffeeType.ESPRESSO;
+                case 2 -> CoffeeType.LATTE;
+                default -> CoffeeType.CAPPUCCINO;
+            };
+            if (waterMilliliters >= coffeeType.WATER_MILLILITERS && milkMilliliters >= coffeeType.MILK_MILLILITERS && coffeeBeansGram >= coffeeType.COFFEE_BEANS_GRAM && disposableCupsNumber >= 1) {
+                System.out.println("I have enough resources, making you a coffee!");
+                disposableCupsNumber--;
+                waterMilliliters -= coffeeType.WATER_MILLILITERS;
+                milkMilliliters -= coffeeType.MILK_MILLILITERS;
+                coffeeBeansGram -= coffeeType.COFFEE_BEANS_GRAM;
+                moneyAmount += coffeeType.COST;
+            } else if (waterMilliliters < coffeeType.WATER_MILLILITERS) {
+                System.out.println("Sorry, not enough water!");
+            } else if (milkMilliliters < coffeeType.MILK_MILLILITERS) {
+                System.out.println("Sorry, not enough milk!");
+            } else if (coffeeBeansGram < coffeeType.COFFEE_BEANS_GRAM) {
+                System.out.println("Sorry, not enough coffee beans!");
+            } else {
+                System.out.println("Sorry, not enough disposable cups!");
+            }
+        }
     }
 
     private void fill() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write how many ml of water you want to add:");
+        System.out.println("\nWrite how many ml of water you want to add:");
         waterMilliliters += scanner.nextInt();
         System.out.println("Write how many ml of milk you want to add:");
         milkMilliliters += scanner.nextInt();
@@ -76,10 +82,22 @@ public class CoffeeMachine {
         System.out.println("I gave you $" + moneyAmount);
         moneyAmount = 0;
     }
+
+    private void showInformation() {
+        System.out.printf("""
+                
+                The coffee machine has:
+                %d ml of water
+                %d ml of milk
+                %d g of coffee beans
+                %d disposable cups
+                $%d of money
+                """, waterMilliliters, milkMilliliters, coffeeBeansGram, disposableCupsNumber, moneyAmount);
+    }
 }
 
 enum Action {
-    BUY, FILL, TAKE
+    BUY, FILL, TAKE, REMAINING, EXIT
 }
 
 enum CoffeeType {
