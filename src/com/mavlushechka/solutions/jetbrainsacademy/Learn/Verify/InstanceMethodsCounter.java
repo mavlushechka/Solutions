8db2e194e6f9324395a258abd987b18ff2480774
Mavlushechka <mavlushechka@gmail.com>
2021-12-12 21:29:16 +0500

    Solve InstanceMethodsCounter problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Verify/InstanceMethodsCounter.java
@@ -0,0 +1,17 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Verify;
+
+public class InstanceMethodsCounter {
+
+}
+
+class Counter {
+    public int current;
+
+    public void inc() {
+        current++;
+    }
+
+    public int getCurrent() {
+        return current;
+    }
+}
