ada03b4ea0a1e100344091bc0fd9e8bfdf95b87a
Mavlushechka <mavlushechka@gmail.com>
2021-12-16 16:14:36 +0500

    Solve TypesAndVariablesInvalidDeclarationOfVariables problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/TypesAndVariablesInvalidDeclarationOfVariables.java
@@ -0,0 +1,10 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+public class TypesAndVariablesInvalidDeclarationOfVariables {
+    public static void main(String[] args) {
+        int a = 512343;
+        int b = 3431231;
+
+        System.out.println(a + b);
+    }
+}
