6996f73b5206eb3eae74357624f448e662ee8968
Mavlushechka <mavlushechka@gmail.com>
2021-12-21 16:03:14 +0500

    Refactor

--- src/com/mavlushechka/solutions/university/_6/_0/_8.java
+++ src/com/mavlushechka/solutions/university/_6/_0/_8.java
@@ -1,36 +1,26 @@
 package com.mavlushechka.solutions.university._6._0;
 
 class Safe {
-
-    private String encryptedText;
-
-    private final String decryptedText;
-
-    private final int step;
+    public final String encryptedText;
+    public final String decryptedText;
+    public final int step;
 
     public Safe(String decryptedText, int step) {
         this.decryptedText = decryptedText;
         this.step = step;
-        Encrypt();
-    }
 
-    private void Encrypt() {
         StringBuilder encryptedText = new StringBuilder();
-        for (int i = 0; i < this.decryptedText.length(); i ++) {
-            encryptedText.append((char) (this.decryptedText.charAt(i) + step));
+        for (int i = 0; i < decryptedText.length(); i++) {
+            encryptedText.append((char) (decryptedText.charAt(i) + step));
         }
         this.encryptedText = encryptedText.toString();
-    }
-
-    @Override
-    public String toString() {
-        return encryptedText;
     }
 }
 
 public class _8 {
-
     public static void main(String[] args) {
-        System.out.println(new Safe("Java", 3));
+        Safe safe = new Safe("Java", 3);
+        System.out.println(safe.encryptedText);
+        System.out.println(safe.decryptedText);
     }
 }
