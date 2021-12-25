1c3f84ea6a52214d3e34090662da1a55cb6d33d2
Mavlushechka <mavlushechka@gmail.com>
2021-12-26 02:43:32 +0500

    Solve BoxingAndUnboxingSafeConverting problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/BoxingAndUnboxingSafeConverting.java
@@ -0,0 +1,25 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+import java.util.Scanner;
+
+public class BoxingAndUnboxingSafeConverting {
+
+    public static int convert(Long val) {
+        if (val == null) {
+            return 0;
+        } else if (val > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        } else if (val < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        } else {
+            return val.intValue();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String val = scanner.nextLine();
+        Long longVal = "null".equals(val) ? null : Long.parseLong(val);
+        System.out.println(convert(longVal));
+    }
+}
