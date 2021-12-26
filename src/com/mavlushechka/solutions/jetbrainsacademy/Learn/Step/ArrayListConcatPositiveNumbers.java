528e4ee30543e6896457b4df21a49364c29b04b1
Mavlushechka <mavlushechka@gmail.com>
2021-12-26 19:56:50 +0500

    Solve ArrayListConcatPositiveNumbers problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/ArrayListConcatPositiveNumbers.java
@@ -0,0 +1,44 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ArrayListConcatPositiveNumbers {
+
+    public static ArrayList<Integer> concatPositiveNumbers(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (Integer number : l1) {
+            if (number > 0) {
+                list.add(number);
+            }
+        }
+        for (Integer number : l2) {
+            if (number > 0) {
+                list.add(number);
+            }
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list1 = readArrayList(scanner);
+        ArrayList<Integer> list2 = readArrayList(scanner);
+
+        ArrayList<Integer> result = concatPositiveNumbers(list1, list2);
+
+        result.forEach(n -> System.out.print(n + " "));
+    }
+
+    private static ArrayList<Integer> readArrayList(Scanner scanner) {
+        return Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+}
