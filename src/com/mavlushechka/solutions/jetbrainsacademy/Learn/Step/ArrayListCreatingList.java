ef8babd62106bf4d62943cddc62353d243616621
Mavlushechka <mavlushechka@gmail.com>
2021-12-29 04:29:03 +0500

    Solve ArrayListCreatingList problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/ArrayListCreatingList.java
@@ -0,0 +1,12 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListCreatingList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(List.of(2, 0, 1, 7));
+
+        System.out.println(list);
+    }
+}
