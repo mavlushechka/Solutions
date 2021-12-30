4cc897073e9f04fadcf29cede3b42cdf6aedaead
Mavlushechka <mavlushechka@gmail.com>
2021-12-30 15:46:31 +0500

    Solve CommentsIncorrectComments problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Daily/CommentsIncorrectComments.java
@@ -0,0 +1,7 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Daily;
+
+public class CommentsIncorrectComments {
+    public static void main(String[] args) {
+        System.out.println("Yet another simple Java application");
+    }
+}
