fde7f14c1c100ffd4f7e7bd700d73148d0d7aa92
Mavlushechka <mavlushechka@gmail.com>
2021-12-31 03:21:43 +0500

    Refactor

--- src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Daily/SwitchStatementFloorSpaceOfTheRoom.java
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Daily/SwitchStatementFloorSpaceOfTheRoom.java
@@ -5,30 +5,31 @@ import java.util.Scanner;
 public class SwitchStatementFloorSpaceOfTheRoom {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double pi = 3.14;
         String type = scanner.next();
-        double[] parameters;
         double square;
 
-        if ("triangle".equals(type)) {
-            parameters = new double[3];
-        } else if ("rectangle".equals(type)) {
-            parameters = new double[2];
-        } else {
-            parameters = new double[1];
-        }
-        for (int i = 0; i < parameters.length; i++) {
-            parameters[i] = scanner.nextDouble();
-        }
+        switch (type) {
+            case "triangle" -> {
+                double a = scanner.nextDouble();
+                double b = scanner.nextDouble();
+                double c = scanner.nextDouble();
 
-        if ("triangle".equals(type)) {
-            double perimeter = (parameters[0] + parameters[1] + parameters[2]) / 2;
-            square = Math.sqrt(perimeter * (perimeter - parameters[0])
-                    * (perimeter - parameters[1]) * (perimeter - parameters[2]));
-        } else if ("rectangle".equals(type)) {
-            square = parameters[0] * parameters[1];
-        } else {
-            square = pi * parameters[0] * parameters[0];
+                double perimeter = (a + b + c) / 2;
+                square = Math.sqrt(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c));
+            }
+            case "rectangle" -> {
+                double a = scanner.nextDouble();
+                double b = scanner.nextDouble();
+
+                square = a * b;
+            }
+            case "circle" -> {
+                double a = scanner.nextDouble();
+
+                final double pi = 3.14;
+                square = pi * a * a;
+            }
+            default -> square = 0;
         }
 
         System.out.println(square);
