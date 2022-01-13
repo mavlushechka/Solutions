9a3e06353c6c6cc7f196d984428cbdb2c31c7728
Mavlushechka <mavlushechka@gmail.com>
2022-01-13 10:32:01 +0500

    Solve TheMainMethodFixingErrorsInMain problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Daily/TheMainMethodFixingErrorsInMain.java
@@ -0,0 +1,7 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Daily;
+
+public class TheMainMethodFixingErrorsInMain {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}
