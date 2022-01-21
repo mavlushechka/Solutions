7f9eacee2642e9d8a3ecd929f4fe35bb566da8e3
Mavlushechka <mavlushechka@gmail.com>
2022-01-21 06:24:44 +0500

    Some refactor

--- src/com/mavlushechka/solutions/javaabeginnersguide/_5_3/ShowBits.java
+++ src/com/mavlushechka/solutions/javaabeginnersguide/_5_3/ShowBits.java
@@ -8,18 +8,8 @@ public class ShowBits {
     }
 
     public void show(long val) {
-        long mask = 1;
-
-        // left-shift a 1 into the proper position
-        mask <<= numberOfBits -1;
-
-        int spacer = 0;
-        for (; mask != 0; mask >>>= 1) {
-            if ((val & mask) != 0) {
-                System.out.print("1");
-            } else {
-                System.out.print("0");
-            }
+        for (long mask = 1L << numberOfBits -1, spacer = 0; mask != 0; mask >>>= 1) {
+            System.out.print(((val & mask) != 0) ? "1" : "0");
             spacer++;
             if ((spacer % 8) == 0) {
                 System.out.print(" ");
