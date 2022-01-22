4d0dfb82f2678d211e6d77107991f2a27de5b4b3
Mavlushechka <mavlushechka@gmail.com>
2022-01-22 06:05:18 +0500

    Add WriteDemo learning solution

---
+++ src/com/mavlushechka/solutions/learning/WriteDemo.java
@@ -0,0 +1,10 @@
+package com.mavlushechka.solutions.learning;
+
+public class WriteDemo {
+    public static void main(String[] args) {
+        int b = 'X';
+
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
