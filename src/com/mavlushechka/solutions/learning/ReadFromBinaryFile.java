729680433af785bea189fc9293f5cf21e0ab9c2b
Mavlushechka <mavlushechka@gmail.com>
2022-01-22 09:29:10 +0500

    Add reading and writing binary file learning solutions

---
+++ src/com/mavlushechka/solutions/learning/ReadFromBinaryFile.java
@@ -0,0 +1,16 @@
+package com.mavlushechka.solutions.learning;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadFromBinaryFile {
+    public static void main(String[] args) throws IOException {
+        DataInputStream dataInputStream = new DataInputStream(new FileInputStream("src/com/mavlushechka/solutions/learning/BinaryFile.txt"));
+
+        System.out.println(dataInputStream.readInt());
+        System.out.println(dataInputStream.readBoolean());
+
+        dataInputStream.close();
+    }
+}
