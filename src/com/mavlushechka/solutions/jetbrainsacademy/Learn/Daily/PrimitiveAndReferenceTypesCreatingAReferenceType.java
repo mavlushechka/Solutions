50a39ee18aa59df508272006b0f01bf9ca432610
Mavlushechka <mavlushechka@gmail.com>
2022-01-23 16:16:12 +0500

    Solve PrimitiveAndReferenceTypesCreatingAReferenceType problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Daily/PrimitiveAndReferenceTypesCreatingAReferenceType.java
@@ -0,0 +1,9 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Daily;
+
+public class PrimitiveAndReferenceTypesCreatingAReferenceType {
+    public static void main(String[] args) {
+        String text = "Hello, learner!";
+
+        System.out.println(text);
+    }
+}
