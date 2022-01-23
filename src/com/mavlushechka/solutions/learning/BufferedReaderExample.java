f4eac49821b3903ebe372038e137f753983bf1e2
Mavlushechka <mavlushechka@gmail.com>
2022-01-23 07:47:12 +0500

    Add readLine() example

--- src/com/mavlushechka/solutions/learning/BufferedReaderExample.java
+++ src/com/mavlushechka/solutions/learning/BufferedReaderExample.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ReadChars {
+public class BufferedReaderExample {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
@@ -12,6 +12,10 @@ public class ReadChars {
             System.out.print(character);
         }
         System.out.print(".\n");
+
+        for (String string = bufferedReader.readLine(); !"stop".equalsIgnoreCase(string); string = bufferedReader.readLine()) {
+            System.out.println(string);
+        }
 
         bufferedReader.close();
     }
