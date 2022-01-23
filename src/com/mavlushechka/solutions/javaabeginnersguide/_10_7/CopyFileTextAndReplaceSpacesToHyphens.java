706878014c5c501d2ae6e6694b4d3dfdcf71f818
Mavlushechka <mavlushechka@gmail.com>
2022-01-23 15:12:41 +0500

    Solve CopyFileTextAndReplaceSpacesToHyphens solution

---
+++ src/com/mavlushechka/solutions/javaabeginnersguide/_10_7/CopyFileTextAndReplaceSpacesToHyphens.java
@@ -0,0 +1,20 @@
+package com.mavlushechka.solutions.javaabeginnersguide._10_7;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyFileTextAndReplaceSpacesToHyphens {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("src/com/mavlushechka/solutions/javaabeginnersguide/_10_7/File.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream("src/com/mavlushechka/solutions/javaabeginnersguide/_10_7/File (copy).txt");
+
+        for (char character = (char) fileInputStream.read(); (short) character != -1; character = (char) fileInputStream.read()) {
+            fileOutputStream.write(character == ' ' ? '-' : character);
+        }
+        System.out.println("Process completed!");
+
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
