cc8c287ca77e66a1e14e13ce2be7fb39bfc276e6
Mavlushechka <mavlushechka@gmail.com>
2022-01-23 10:56:51 +0500

    Solve UnicodeDemo learning solution

---
+++ src/com/mavlushechka/solutions/learning/UnicodeDemo.java
@@ -0,0 +1,9 @@
+package com.mavlushechka.solutions.learning;
+
+public class UnicodeDemo {
+    public static void main(String[] args) {
+        char unicode = '\u2211';
+
+        System.out.println(unicode);
+    }
+}
