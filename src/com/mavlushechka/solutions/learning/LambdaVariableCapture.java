5c2f1e89d096f4ef14e59baacfb27320173e1eff
Mavlushechka <mavlushechka@gmail.com>
2022-01-29 14:10:39 +0500

    Add static int field

--- src/com/mavlushechka/solutions/learning/LambdaVariableCapture.java
+++ src/com/mavlushechka/solutions/learning/LambdaVariableCapture.java
@@ -5,19 +5,22 @@ interface MyFunc {
 }
 
 public class LambdaVariableCapture {
+    public static int field = 5;
+
     public static void main(String[] args) {
         int number = 10;
 
-        MyFunc increaseBy10 = n -> {
+        MyFunc increaseBy15 = n -> {
             // Variable used in lambda expression should be final or effectively final
             // number = 11;
 
-            return n + number;
+            return n + number + field;
         };
 
-        System.out.println(increaseBy10.func(8));
+        System.out.println(increaseBy15.func(8));
 
         // Variable used in lambda expression should be final or effectively final
         // number = 11;
+        field = 6;
     }
 }
