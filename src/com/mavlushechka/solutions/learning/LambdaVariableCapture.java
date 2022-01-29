eef10519936825530d6b3efd3be0dce7fe0129f0
Mavlushechka <mavlushechka@gmail.com>
2022-01-29 14:37:21 +0500

    Add ISystemInRead interface

--- src/com/mavlushechka/solutions/learning/LambdaVariableCapture.java
+++ src/com/mavlushechka/solutions/learning/LambdaVariableCapture.java
@@ -1,7 +1,13 @@
 package com.mavlushechka.solutions.learning;
 
+import java.io.IOException;
+
 interface MyFunc {
     int func(int n);
+}
+
+interface ISystemInRead {
+    char func() throws IOException;
 }
 
 public class LambdaVariableCapture {
@@ -16,8 +22,14 @@ public class LambdaVariableCapture {
 
             return n + number + field;
         };
+        ISystemInRead iSystemInRead = () -> (char) System.in.read();
 
         System.out.println(increaseBy15.func(8));
+        try {
+            System.out.println(iSystemInRead.func());
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
 
         // Variable used in lambda expression should be final or effectively final
         // number = 11;
