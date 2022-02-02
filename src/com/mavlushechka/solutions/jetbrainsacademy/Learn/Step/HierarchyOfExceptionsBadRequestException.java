992023b7d441cf114a5a19cb299d0060fad92039
Mavlushechka <mavlushechka@gmail.com>
2022-02-02 05:49:26 +0500

    Solve HierarchyOfExceptionsBadRequestException problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Step/HierarchyOfExceptionsBadRequestException.java
@@ -0,0 +1,7 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Step;
+
+public class HierarchyOfExceptionsBadRequestException extends Exception {
+    public HierarchyOfExceptionsBadRequestException(String message) {
+        super(message);
+    }
+}
