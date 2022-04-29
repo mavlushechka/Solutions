f0b91ced0cd9cd5e244e4fe1adafd6d2f782cfe7
Mavlushechka <mavlushechka@gmail.com>
2022-04-29 11:04:23 +0500

    Solve PatientNeedsADoctor problem

---
+++ src/com/mavlushechka/solutions/jetbrainsacademy/Learn/Daily/PatientNeedsADoctor.java
@@ -0,0 +1,12 @@
+package com.mavlushechka.solutions.jetbrainsacademy.Learn.Daily;
+
+public class PatientNeedsADoctor {
+}
+
+class Patient {
+    public String name;
+
+    public void say() {
+        System.out.println("Hello, my name is " + name + ", I need a doctor.");
+    }
+}
