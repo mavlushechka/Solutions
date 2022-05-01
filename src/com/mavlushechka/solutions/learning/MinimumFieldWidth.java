8b2ed16f335f6c1b4d2662ca92d1f4e817c488f9
Mavlushechka <mavlushechka@gmail.com>
2022-05-01 22:48:06 +0500

    Solve MinimumFieldWidth learning problem

---
+++ src/com/mavlushechka/solutions/learning/MinimumFieldWidth.java
@@ -0,0 +1,9 @@
+package com.mavlushechka.solutions.learning;
+
+public class MinimumFieldWidth {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%4d %4d %4d\n", i, i*i, i*i*i);
+        }
+    }
+}
