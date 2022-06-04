b84aed8d37d2c4739b1cedb28bc3e94ed995523b
Mavlushechka <mavlushechka@gmail.com>
2022-06-05 00:50:59 +0500

    Rename class from Scanner to ScannerDemo

--- src/com/mavlushechka/solutions/learning/ScannerDemo.java
+++ src/com/mavlushechka/solutions/learning/ScannerDemo.java
@@ -1,6 +1,6 @@
 package com.mavlushechka.solutions.learning;
 
-public class Scanner {
+public class ScannerDemo {
     public static void main(String[] args) {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
 
